8f495aeec908577788dfa505e3bca5f5b75ee9cf
oushanskeen <inta.soul@gmail.com>
2021-04-07 23:00:23 +0300

    My commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,61 @@
-import { of, from } from 'rxjs';
+import { fromEvent, of, from } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import {
+  switchMap,
+  catchError,
+  filter,
+  debounceTime,
+  distinctUntilChanged,
+  mergeMap,
+  map
+} from "rxjs/operators";
 
-const o = of(5) // Promise.resolve(5)
+// select observable DOM elements and relevant action
+const selectObservableElement = id => document.getElementById(id);
+const makeObservableBox = watchIt => fromEvent(watchIt, "input");
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+// element 1
+const observeInputOne = selectObservableElement("inputOne");
+const githubInput = makeObservableBox(observeInputOne);
+// element 2
+const observeInputTwo = selectObservableElement("inputTwo");
+const gitlabInput = makeObservableBox(observeInputTwo);
+
+// urls
+const githubRequestUri = `https://api.github.com/search/repositories?q=`;
+const gitlabRequestUri = `https://gitlab.com/api/v4/projects?search=`;
+
+// request function
+const makeSearchRequest = uri => request => `${uri}${request}`;
+
+const githubRequest = makeSearchRequest(githubRequestUri);
+const gitlabRequest = makeSearchRequest(gitlabRequestUri);
+
+// fetch function
+const fetchPipe = action =>
+  switchMap((event: KeyboardEvent) => {
+    return fetch(` ${action((event.target as HTMLInputElement).value)}`).then(
+      res => res.json()
+    );
+  });
+
+// useless filter
+const filterPipe = () => filter(v => Object.keys(v).length > 5);
+
+// universl observable factory
+const searchObservable = (observableElement, observableAction) =>
+  observableElement
+    .pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      fetchPipe(observableAction),
+      map(value => JSON.stringify(value))
+    )
+    .subscribe({
+      next: res => console.log(res),
+      error: console.log
+    });
+
+// trigger observable subscription
+searchObservable(githubInput, githubRequest);
+searchObservable(gitlabInput, gitlabRequest);
